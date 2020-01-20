d47761f056b234c7cdc5782ea65a066a31a8b3a3
Akshay Jain <41275651+akshayjain98@users.noreply.github.com>
2020-01-20 15:36:38 +0530

    Update ConnectionModel.py

--- Database/ConnectionModel.py
+++ Database/ConnectionModel.py
@@ -5,14 +5,5 @@ class ConnectionModel:
     @staticmethod
     def connect(collection_name):
         #without Authentication
-        #connection_url = MongoClient("localhost", 27017)
-
-
-        #with authentication
-
-        connection_url = MongoClient(host="localhost", username="akshay.jain", password="akshay123", port=27017)
-
-        #connection_url = MongoClient("mongodb://akshay.jain:akshay123@localhost:27017/aj_bank?authSource=admin")
-
-        #connection_url = MongoClient("mongodb://akshay.jain:akshay123@localhost:27017/")
+        connection_url = MongoClient("localhost", 27017)
         return connection_url["aj_bank"][collection_name]
